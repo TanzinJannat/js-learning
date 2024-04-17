302e020adcca5b8dcb220996a6f5130946ff9e66
TanzinJannat <161127740+TanzinJannat@users.noreply.github.com>
2024-04-17 06:28:32 +0000

    file

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,11 @@
 console.log('Tanzin');
+const accountId = 1442435;
+let accountEmail = "Tanzin@gmail.com";
+var accountPassword = "omin420";
+accountCity = "Feni";
+
+
+
+
+accountEmail = "jannat@gmail.com";
+console.log(accountEmail);
