0817a3cd5728a9548a5344490761ba519ecb7fcb
TanzinJannat <161127740+TanzinJannat@users.noreply.github.com>
2024-04-17 06:55:54 +0000

    new file

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+console.log('Tanzin');
+const accountId = 1442435;
+let accountEmail = "Tanzin@gmail.com";
+var accountPassword = "omin420";
+accountCity = "Feni";
+let accountState;
+
+
+// accountId = 45465476;
+// console.log(accountId); not allowed
+
+accountEmail = "jannat@gmail.com";
+accountPassword = "jannat420";
+accountCity = "Dhaka";
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+/*
+prefer not to use var bacause of issue in block scope and functional scope
+*/
