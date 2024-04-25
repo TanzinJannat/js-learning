a53f72a1ff2d848b068461c45c22cf4688f79fe7
TanzinJannat <161127740+TanzinJannat@users.noreply.github.com>
2024-04-25 18:22:45 +0000

    new file

---
+++ 01_basics/06_string.js
@@ -0,0 +1,23 @@
+const name = "Tanzin";
+const repoCount = 50;
+// console.log(`My name is ${name} & My repo count is ${repoCount}`);
+
+const gameName = new String("hitesh-hc");
+// console.log(gameName);
+
+// console.log(gameName[3]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(3));
+
+// console.log(gameName.indexOf('z'));
+
+let newString = gameName.substring(0,4);
+newString = gameName.substring(0, 5);
+newString = gameName.substring(0,  6);
+// console.log(newString);
+
+let anotherString = gameName.slice(-8, 4);
+
+console.log(anotherString);
