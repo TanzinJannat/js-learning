0832401e68d80f85b501d8fb06c142f94ead02a1
TanzinJannat <161127740+TanzinJannat@users.noreply.github.com>
2024-04-26 14:45:48 +0000

    new file

--- 01_basics/06_string.js
+++ 01_basics/06_string.js
@@ -2,7 +2,7 @@ const name = "Tanzin";
 const repoCount = 50;
 // console.log(`My name is ${name} & My repo count is ${repoCount}`);
 
-const gameName = new String("hitesh-hc");
+const gameName = new String("hitesh-hc-com");
 // console.log(gameName);
 
 // console.log(gameName[3]);
@@ -16,8 +16,20 @@ const gameName = new String("hitesh-hc");
 let newString = gameName.substring(0,4);
 newString = gameName.substring(0, 5);
 newString = gameName.substring(0,  6);
-// console.log(newString);
+console.log(newString);
 
 let anotherString = gameName.slice(-8, 4);
-
 console.log(anotherString);
+
+const newStringOne = "    hitesh    ";
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+
+const url = "http://hitesh.com/hitesh%20choudhary"
+
+console.log(url.replace('%20', '-'));
+
+console.log(url.includes('hitesh'));
+
+console.log(gameName.split('-'));
