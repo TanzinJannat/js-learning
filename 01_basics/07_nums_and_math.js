aa5ca0504020e2e9b69d2eb9521ba8f8dd672a14
TanzinJannat <161127740+TanzinJannat@users.noreply.github.com>
2024-05-06 14:54:15 +0000

    new

--- 01_basics/07_nums_and_math.js
+++ 01_basics/07_nums_and_math.js
@@ -22,13 +22,15 @@ const hundreds = 1000000;
 
 //+++++++++++++++++++ Maths ++++++++++++++++++++++++++
 
-// console.log(Math);
+console.log(Math);
 // console.log(Math.abs(-4));
 // console.log(Math.round(4.235));
+// console.log(Math.round(4.6));
 // console.log(Math.ceil(4.235));
 // console.log(Math.floor(4.9));
 // console.log(Math.min(4, 3, 6, 8));
 // console.log(Math.max(4, 3, 6, 8));
 
 
-// console.log(Math.random());
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
