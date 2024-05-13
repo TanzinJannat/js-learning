1234474b3ca302ab36ab6c6b1b1fb79b8850e96e
TanzinJannat <161127740+TanzinJannat@users.noreply.github.com>
2024-05-13 14:13:12 +0000

    new

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,1 +1,24 @@
 // array
+
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeros = ["shoktiman", "spiderman"]
+
+// Another way 
+
+const myArr2 = new Array(0, 1, 2, 3, 4, 5)
+// console.log(myHeros[1]);
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop() //remove last value
+
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3))
+
+// const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log(typeof newArr)
